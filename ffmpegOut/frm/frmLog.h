824e3af0be3cde21b87b7ae50c4b764f374ffe5a
rigaya <rigaya34589@live.jp>
2022-03-18 18:45:43 +0900

    ログの保存に失敗すると、例外が発生していたのを回避。

--- ffmpegOut/frm/frmLog.h
+++ ffmpegOut/frm/frmLog.h
@@ -715,10 +715,15 @@ private: System::Windows::Forms::ToolStripMenuItem^  toolStripMenuItemFilePathOp
                 sw->WriteLine(DateTime::Now.ToString("yyyy年M月d日 H時mm分 エンコード終了"));
                 sw->WriteLine(L"-------------------------------------------------------------------------------------");
                 sw->WriteLine();
-            } finally {
-                if (sw != nullptr) {
-                    sw->Close();
-                }
+                sw->Close();
+            } catch (IOException^ ex) {
+                WriteLogAuoLine("自動ログ保存に失敗しました。", LOG_WARNING);
+                WriteLogAuoLine(ex->Message, LOG_WARNING);
+            } catch (UnauthorizedAccessException^ ex) {
+                WriteLogAuoLine("自動ログ保存に失敗しました。", LOG_WARNING);
+                WriteLogAuoLine(ex->Message, LOG_WARNING);
+            } catch (...) {
+                WriteLogAuoLine("自動ログ保存に失敗しました。", LOG_WARNING);
             }
         }
     public:
