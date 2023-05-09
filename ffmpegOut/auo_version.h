7779176d90465ebdd2a75cea4e74252f19bc0220
rigaya <rigaya34589@live.jp>
2023-05-09 22:03:50 +0900

    中国語翻訳を更新。

--- ffmpegOut/auo_version.h
+++ ffmpegOut/auo_version.h
@@ -28,9 +28,9 @@
 #ifndef _AUO_VERSION_H_
 #define _AUO_VERSION_H_
 
-#define AUO_VERSION          0,1,7,0
-#define AUO_VERSION_STR      "1.07"
-#define AUO_VERSION_STR_W   L"1.07"
+#define AUO_VERSION          0,1,8,0
+#define AUO_VERSION_STR      "1.08"
+#define AUO_VERSION_STR_W   L"1.08"
 #define AUO_NAME_R            ffmpegOut
 #define AUO_NAME_WITHOUT_EXT "ffmpegOut"
 #define AUO_NAME_WITHOUT_EXT_W L"ffmpegOut"
