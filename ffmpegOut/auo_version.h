d72d56b2f3532553cf26d2f959602d42f77adff3
rigaya <rigaya34589@live.jp>
2025-12-28 13:15:14 +0900

    カレントディレクトリがAviUtl2.exeがない場所の時の対策を追加。

--- ffmpegOut/auo_version.h
+++ ffmpegOut/auo_version.h
@@ -28,9 +28,9 @@
 #ifndef _AUO_VERSION_H_
 #define _AUO_VERSION_H_
 
-#define AUO_VERSION         0,2,4,0
-#define AUO_VERSION_STR      "2.04"
-#define AUO_VERSION_STR_W   L"2.04"
+#define AUO_VERSION         0,2,5,0
+#define AUO_VERSION_STR      "2.05"
+#define AUO_VERSION_STR_W   L"2.05"
 #define AUO_NAME_R            ffmpegOut
 #define AUO_NAME_WITHOUT_EXT "ffmpegOut"
 #define AUO_NAME_WITHOUT_EXT_W L"ffmpegOut"
